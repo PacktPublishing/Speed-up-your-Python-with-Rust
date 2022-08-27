639f3d5db66aab4201a0346db096208a5533a3b2
MBounouar <MBounouar@users.noreply.github.com>
2022-08-27 17:10:54 +0200

    fixed compiling and warning issues

--- chapter_six/constructing_our_own_python_objects/src/lib.rs
+++ chapter_six/constructing_our_own_python_objects/src/lib.rs
@@ -1,29 +1,26 @@
 use pyo3::prelude::*;
 use pyo3::wrap_pyfunction;
 
-mod interface;
-mod fib_calcs;
 mod class_module;
+mod fib_calcs;
+mod interface;
 
-use fib_calcs::fib_number::__pyo3_get_function_fibonacci_number;
-use fib_calcs::fib_numbers::__pyo3_get_function_fibonacci_numbers;
-use interface::config::__pyo3_get_function_run_config;
 use class_module::fib_processor::FibProcessor;
-
-
+use fib_calcs::fib_number::fibonacci_number;
+use fib_calcs::fib_numbers::fibonacci_numbers;
+use interface::config::run_config;
 
 #[pyfunction]
 fn say_hello() {
     println!("saying hello from Rust!");
 }
 
-
 #[pymodule]
 fn flitton_fib_rs(_py: Python, m: &PyModule) -> PyResult<()> {
-    m.add_wrapped(wrap_pyfunction!(say_hello));
-    m.add_wrapped(wrap_pyfunction!(fibonacci_number));
-    m.add_wrapped(wrap_pyfunction!(fibonacci_numbers));
-    m.add_wrapped(wrap_pyfunction!(run_config));
+    m.add_wrapped(wrap_pyfunction!(say_hello))?;
+    m.add_wrapped(wrap_pyfunction!(fibonacci_number))?;
+    m.add_wrapped(wrap_pyfunction!(fibonacci_numbers))?;
+    m.add_wrapped(wrap_pyfunction!(run_config))?;
     m.add_class::<FibProcessor>()?;
     Ok(())
 }
